427c59de5c0e5d233a2432628feba202d2b6e567
poncho29 <meneses0828@gmail.com>
2025-02-10 12:02:13 -0500

    add: protection of the user module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,30 +12,38 @@ import {
 
 import { UsersService } from './users.service';
 
+import { Auth } from '../auth/decorators';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PaginationDto } from '../common/dtos';
+
+import { ValidRoles } from '../auth/interfaces';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @Auth(ValidRoles.SUPER_ADMIN)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @Auth(ValidRoles.SUPER_ADMIN, ValidRoles.ADMIN)
   findAll(@Query() pagination: PaginationDto) {
     return this.usersService.findAll(pagination);
   }
 
   @Get(':id')
+  @Auth(ValidRoles.SUPER_ADMIN, ValidRoles.ADMIN)
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.SUPER_ADMIN, ValidRoles.ADMIN)
   update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -44,6 +52,7 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.SUPER_ADMIN, ValidRoles.ADMIN)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.remove(id);
   }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,7 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from '../auth/auth.module';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -7,7 +9,7 @@ import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [TypeOrmModule, UsersService],
